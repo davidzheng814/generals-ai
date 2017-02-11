4b4c06dfdc9f9446d8d22ad2302163dab32474a6
edwardpark <edwardpark97@gmail.com>
2017-02-10 19:57:51 -0500

    Added some comments, changed some stuff

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,21 +1,13 @@
 #include "./engine.h"
 
 Game::Game() {
-  this->board = new piece[BOARD_SIZE];
-
-  for (int i = 0; i < NUM_PLAYERS; ++i) {
-    this->vision[i] = new bool[BOARD_SIZE];
-  }
 }
 
 Game::~Game() {
-  delete [] this->board;
-
-  for (int i = 0; i < NUM_PLAYERS; ++i) {
-    delete [] this->vision[i];
-  }
 }
 
+// Generates a new unit on generals and cities at the end of every turn
+// Only works if the current move number is divisible by the (moves per turn)
 void Game::generate() {
   if (this->move % MPT != 0) return;
 
@@ -26,6 +18,19 @@ void Game::generate() {
   }
 }
 
+// Gives the player vision around a certain position
+void Game::create_vision(int player, int position) {
+  for (int dx = -1; dx <= 1; ++dx) {
+    for (int dy = -1; dy <= 1; ++dy) {
+      int new_pos = position + dx * BOARD_WIDTH + dy;
+      if (0 <= new_pos && new_pos < BOARD_SIZE) {
+        this->vision[player][new_pos] = true;
+      }
+    }
+  }
+}
+
+// Initializes all relevant fields at the start of a new game
 void Game::start(vector<int> &mountains,
     vector<int> &cities, vector<int> &generals) {
   for (size_t i = 0; i < BOARD_SIZE; ++i) {
@@ -42,29 +47,35 @@ void Game::start(vector<int> &mountains,
 
   for (size_t i = 0; i < generals.size(); ++i) {
     this->board[generals[i]] = {GENERAL, (int8_t)i, 0};
+    this->create_vision(i, generals[i]);
   }
 
   this->move = 0;
 
   for (int i = 0; i < NUM_PLAYERS; ++i) {
-    is_alive[i] = true;
+    this->is_alive[i] = true;
+    this->num_land[i] = 1;
     ORDER[i] = i;
     ORDER[MPT - i - 1] = i;
   }
 }
 
+// Finds the next player who needs to make a move
 void Game::next_move() {
   while (true) {
-    this->move = (this->move + 1) % NUM_PLAYERS;
+    this->move++;
+    this->generate();
     if (is_alive[player(this->move)]) {
       break;
     }
   }
 }
 
-void Game::make_move() {
+// Makes the given move
+void make_move(int player, int position, direction dir, bool half_move) {
 }
 
+// Determines whether the game is over
 bool Game::is_gameover() {
   for (int i = 0; i < NUM_PLAYERS; ++i) {
     if (is_alive[i]) return false;
--- engine/engine.h
+++ engine/engine.h
@@ -8,12 +8,13 @@ using namespace std;
 
 #define NUM_PLAYERS 2
 #define BOARD_WIDTH 15
-#define BOARD_SIZE BOARD_WIDTH*BOARD_WIDTH
-#define MPT NUM_PLAYERS*2
+#define BOARD_HEIGHT 15
+#define BOARD_SIZE BOARD_WIDTH*BOARD_HEIGHT
+#define MPT NUM_PLAYERS*2  // Moves per Turn
 int ORDER[MPT];
 
-#define player(a) ORDER[a % MPT]
-#define SPAWNS(a) a == GENERAL || a == ARMY || a == CITY
+#define player(a) ORDER[a % MPT]  // Gives the index of the player who needs to move this turn
+#define SPAWNS(a) a == GENERAL || a == CITY  // Whether a type spawns a unit
 
 enum type {
   EMPTY = 0,
@@ -21,6 +22,13 @@ enum type {
   ARMY = 2,
   CITY = 3,
   MOUNTAIN = 4,
+};
+
+enum direction {
+  NORTH = 0,
+  EAST = 1,
+  SOUTH = 2,
+  WEST = 3,
 };
 
 struct piece {
@@ -32,19 +40,23 @@ struct piece {
 class Game {
   public:
     int move;
-    piece *board;
-    bool *vision[NUM_PLAYERS];
+    piece board[BOARD_SIZE];
+    bool vision[NUM_PLAYERS][BOARD_SIZE];
     bool is_alive[NUM_PLAYERS];
+
+    int num_land[NUM_PLAYERS];
+    int num_units[NUM_PLAYERS];
 
     Game();
     ~Game();
     void start(vector<int> &mountains, vector<int> &cities, vector<int> &generals);
-    void make_move();
+    void make_move(int player, int position, direction dir, bool half_move);
     bool is_gameover();
 
   private:
     void generate();
     void next_move();
+    void create_vision(int player, int position);
 };
 
 #endif
